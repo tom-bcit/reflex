50447c5b59c9a76b0f6f758ce0ede789f48926e9
tom-bcit <the18@my.bcit.ca>
2024-01-07 13:15:19 -0800

    Initial Commit

---
+++ reflex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reflex
+//
+//  Created by Tom He on 2024-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ reflex/reflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  reflexApp.swift
+//  reflex
+//
+//  Created by Tom He on 2024-01-07.
+//
+
+import SwiftUI
+
+@main
+struct reflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
